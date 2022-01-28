b17bb7f3a4cff16d8d533483c28a3b5f5aad6f9b
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-28 14:01:34 -0600

    port

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const notes = require('./db/db.json');
 
+
+const PORT = process.env.PORT || 3001;
+
+
 // GET
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001! 🌎`);
+    console.log(`API server now on port ${PORT}! 🌎`);
   });
